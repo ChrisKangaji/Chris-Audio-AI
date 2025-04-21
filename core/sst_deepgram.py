90b56a29b6c3e03161c1a497f8a1a095cdc57c2a
Chris Kangaji <christian.kangaji@live.se>
2025-04-21 22:34:47 +0200

    Reworked core files and main. Added initial imports.

---
+++ core/sst_deepgram.py
@@ -0,0 +1,7 @@
+import asyncio
+import websockets
+import json
+import sounddevice as sd
+import numpy as np
+import os
+from dotenv import load_dotenv
---
+++ core/tts_elevenlabs.py
@@ -0,0 +1,6 @@
+import requests
+import sounddevice as sd
+import numpy as np
+import json
+import os
+from dotenv import load_dotenv
